ddd5e1a94bd0332ff0b3ad0e549dc7087c019ac2
Chung Vu <chung.vd@tripi.vn>
2021-12-14 00:05:06 +0700

    Create menu

--- src/App.js
+++ src/App.js
@@ -1,59 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { Layout, Avatar, Menu, Breadcrumb, Button } from 'antd';
-import Icon from '@ant-design/icons';
-import Title from 'antd/lib/typography/Title';
-import SubMenu from 'antd/lib/menu/SubMenu';
-// import { Cricketer, ODICareer, Batting, Bowling, TestCareer } from './Cricketer';
-// import CareerDetails from './CareerDetails';
-
-const { Header, Footer, Sider, Content } = Layout;
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import LoginForm from './login';
+import Products from './pages/Products';
 
 function App() {
-  const [selectedPlayer, setSelectedPlayer] = useState('');
-  const [visible, setVisible] = useState(false);
-  const onSelect = name => {
-    setSelectedPlayer(name);
-    setVisible(true);
-  }
-
-  const onClose = () => setVisible(false);
   return (
-    <div className="App">
-      <Layout style={{ height: '100vh' }}>
-        <Header style={{ padding: 10 }}>
-          <Title style={{ color: 'white', textAlign: 'center' }} level={3}>Quản lý thí sinh</Title>
-        </Header>
-        <Layout>
-          <Sider>
-            <Menu defaultSelectedKeys={['Dashboard']} mode="inline">
-              <Menu.Item key='Dashboard'>
-                Dashboard
-              </Menu.Item>
-              <SubMenu
-                title={
-                  <span>
-                    <Icon type="mail" />
-                    <span>Thí sinh</span>
-                  </span>
-                }
-              >
-                <Menu.ItemGroup key='examinee'>
-                  <Menu.Item key='create-examinee'> Thêm thí sinh</Menu.Item>
-                  <Menu.Item key='import-examinee'> Import từ excel</Menu.Item>
-                </Menu.ItemGroup>
-              </SubMenu>
-            </Menu>
-          </Sider>
-          <Layout>
-            <Content style={{ padding: '0 50px' }}>
-
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Phần mềm quản lý thí sinh nhập học</Footer>
-          </Layout>
-        </Layout>
-      </Layout>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/reports' element={<Reports />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/login' element={<LoginForm />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import LoginForm from './login';
+import Home from './pages/Home';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
